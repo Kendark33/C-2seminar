c5e491c6da63cedd3a2e3d471ddb8305471468f5
Kendark33 <you@example.com>
2022-10-08 19:32:01 +0300

    Защита от дурака и новые знания

---
+++ Example06_Dop/Program.cs
@@ -0,0 +1,34 @@
+﻿// Защита на дурака
+
+int GetNumber(string message)
+{
+int result = 0;
+bool isCorrect = false;
+
+while(!isCorrect)
+{
+Console.WriteLine(message);
+
+isCorrect = int.TryParse(Console.ReadLine(), out result);
+
+if(!isCorrect)
+Console.WriteLine("Неверный ввод, введите корректное число");
+
+Console.WriteLine();
+}
+
+return result;
+}
+
+int number = GetNumber("Введите число");
+
+
+/* 
+reference (Ссылочные) - string, class, list, array, dictionary...
+value (значемые) - datetime, int, enum, struct, decimal, long, double, bool...
+
+стек и управляемая куча
+
+стек - значимые значения
+управляемая куча -ссылочные
+ */
